d02905849b9e72c34cd02f741e907f28dfdcfb87
Mika Pflüger <mika.pflueger@climate-resource.com>
2024-01-11 14:33:27 +0100

    test: rename test

--- tests/unit/test_meta.py
+++ tests/unit/test_meta.py
@@ -1,11 +1,11 @@
-"""Tests for ScmRun.meta and related functionality."""
+"""Tests for ScmRun.meta."""
 
 import pandas as pd
 import numpy as np
 
 import scmdata
 
-def test_preserve_type():
+def test_correct_type_meta():
     df = pd.DataFrame({"unit": [np.nan, np.nan], "variable": ["1", "2"], 2015: [1., 2.]}, dtype=str)
     run = scmdata.run.BaseScmRun(pd.DataFrame(df))
     assert run.meta.dtypes["variable"] in (str, object)
